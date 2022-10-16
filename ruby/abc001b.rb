ae9aa4736a70481368087e3214acf4b65f4c65eb
hiromisugie <lozz.gggg@gmail.com>
2022-10-17 06:05:17 +0900

    001bの更新

--- ruby/abc001b.rb
+++ ruby/abc001b.rb
@@ -31,7 +31,105 @@ end
 
 
 # 回答3
+def main
+  m = read_num
+  puts judge_vv(m)
+end
 
+def judge_vv(m)
+  case m
+    when 0...0.1
+      format("%02d", 0)
+    when 0.1..5
+      format("%02d", m * 10)
+    when 6..30
+      format("%02d", m + 50)
+    when 35..70
+      format("%02d", (m - 30) / 5 + 80)
+    when 70.001..100
+      89
+  end
+end
+
+def read_num
+  gets.to_f / 1000
+end
+
+main
 
 
 # 回答4
+def main
+  m = read_num
+  puts judge_vv(m)
+end
+
+def judge_vv(m)
+  fill = "%02d"
+  case m
+    when 0...0.1
+      format(fill, 0)
+    when 0.1..5
+      format(fill, m * 10)
+    when 6..30
+      format(fill, m + 50)
+    when 35..70
+      format(fill, (m - 30) / 5 + 80)
+    when 70.001..100
+      89
+  end
+end
+
+def read_num
+  gets.to_f / 1000
+end
+
+main
+
+
+# 回答5
+m = gets.to_f / 1000
+
+case m
+  when 0...0.1
+    v = 0
+  when 0.1..5
+    v = m * 10
+  when 6..30
+    v = m + 50
+  when 35..70
+    v =  (m - 30) / 5 + 80
+  when 70.001..100
+    v = 89
+end
+
+puts format("%02d", v)
+
+
+
+# 回答6
+def main
+  m = read_num
+  puts format("%02d", judge_vv(m))
+end
+
+def judge_vv(m)
+  case m
+    when 0...0.1
+      0
+    when 0.1..5
+      m * 10
+    when 6..30
+      m + 50
+    when 35..70
+      (m - 30) / 5 + 80
+    when 70.001..100
+      89
+  end
+end
+
+def read_num
+  gets.to_f / 1000
+end
+
+main
